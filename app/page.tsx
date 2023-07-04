99ccf668a99018612c68ad5889f58782c78c1390
Mark Dembo <mdembo@cloudflare.com>
2023-07-04 09:36:02 +0200

    Add backend url input

--- app/page.tsx
+++ app/page.tsx
@@ -6,10 +6,15 @@ export default function FileUploadForm() {
   const [isLoading, setIsLoading] = useState(false);
   const [downloadLink, setDownloadLink] = useState<string | null>(null);
   const [isCopied, setIsCopied] = useState(false);
+  const [backendURL, setBackendURL] = useState("");
 
   const handleFileChange = (event: ChangeEvent<HTMLInputElement>) => {
     const selectedFile = event.target.files?.[0];
     setFile(selectedFile || null);
+  };
+
+  const handleURLChange = (event: ChangeEvent<HTMLInputElement>) => {
+    setBackendURL(event.target.value);
   };
 
   const handleSubmit = async (event: FormEvent) => {
@@ -22,14 +27,10 @@ export default function FileUploadForm() {
       formData.append("file", file);
 
       try {
-        const response = await fetch(
-          "https://backendcm.not-a-single-bug.workers.dev",
-          //"https://webhook.site/04c7c9ce-2f84-4608-b477-b30a0453740b",
-          {
-            method: "POST",
-            body: formData,
-          }
-        );
+        const response = await fetch(backendURL, {
+          method: "POST",
+          body: formData,
+        });
 
         if (response.ok) {
           const downloadLink = await response.text();
@@ -57,50 +58,68 @@ export default function FileUploadForm() {
   };
 
   return (
-    <div className="min-h-screen flex items-center justify-center bg-gray-100">
-      <div className="max-w-md w-full p-6 bg-white rounded-lg shadow-md">
-        <form onSubmit={handleSubmit}>
-          <div className="mb-4">
-            <label
-              htmlFor="fileInput"
-              className="block text-lg font-medium text-gray-700"
-            >
-              Choose a file to upload:
-            </label>
-            <input
-              id="fileInput"
-              type="file"
-              onChange={handleFileChange}
-              className="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
-            />
-          </div>
-          <button
-            type="submit"
-            disabled={isLoading}
-            className={`w-full inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-500 hover:bg-indigo-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 ${
-              isLoading ? "opacity-50 cursor-not-allowed" : ""
-            }`}
-          >
-            {isLoading ? "Uploading..." : "Upload"}
-          </button>
-        </form>
-        {downloadLink && (
-          <div>
-            <label>Download Link:</label>
-            <div className="flex flex-col">
-              <input
-                type="text"
-                value={downloadLink}
-                readOnly
-                className=" bg-gray-300"
-              />
-              <button className="" onClick={handleCopyLink}>
-                {isCopied ? "Copied!" : "Copy to Clipboard"}
-              </button>
-            </div>
-          </div>
-        )}
+    <>
+      <div className="sticky top-0 bg-white flex items-center justify-center border-black border-b-2 py-2 px-4">
+        <label
+          htmlFor="backendInput"
+          className="block text-lg font-medium text-gray-700 pr-4"
+        >
+          Set backend URL:
+        </label>
+        <input
+          type="text"
+          id="backendInput"
+          value={backendURL}
+          onChange={handleURLChange}
+          className="border border-gray-300 rounded-md px-2 py-1 w-3/4"
+          placeholder="Enter backend URL"
+        />
       </div>
-    </div>
+      <div className="min-h-screen flex items-center justify-center bg-gray-100">
+        <div className="max-w-md w-full p-6 bg-white rounded-lg shadow-md">
+          <form onSubmit={handleSubmit}>
+            <div className="mb-4">
+              <label
+                htmlFor="fileInput"
+                className="block text-lg font-medium text-gray-700"
+              >
+                Choose a file to upload:
+              </label>
+              <input
+                id="fileInput"
+                type="file"
+                onChange={handleFileChange}
+                className="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
+              />
+            </div>
+            <button
+              type="submit"
+              disabled={isLoading}
+              className={`w-full inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-500 hover:bg-indigo-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 ${
+                isLoading ? "opacity-50 cursor-not-allowed" : ""
+              }`}
+            >
+              {isLoading ? "Uploading..." : "Upload"}
+            </button>
+          </form>
+          {downloadLink && (
+            <div>
+              <label>Download Link:</label>
+              <div className="flex flex-col">
+                <input
+                  type="text"
+                  value={downloadLink}
+                  readOnly
+                  className=" bg-gray-300"
+                />
+                <button className="" onClick={handleCopyLink}>
+                  {isCopied ? "Copied!" : "Copy to Clipboard"}
+                </button>
+              </div>
+            </div>
+          )}
+        </div>
+      </div>
+    </>
   );
 }
